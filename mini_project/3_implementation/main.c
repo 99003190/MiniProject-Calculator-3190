a4be8f1f06864a0ba27d1d41448e10f55ceb6112
99003192 <75420528+99003192@users.noreply.github.com>
2020-12-07 11:16:07 +0530

    Update main.c

--- mini_project/3_implementation/main.c
+++ mini_project/3_implementation/main.c
@@ -23,7 +23,7 @@ void calculator_menu(void)
     printf("\nAvailable Operations\n");
     printf("\n1. Arithmetic\n2. Trigonometric\n3. Other\n4. Exit\t");
     scanf("%d", &choice);
-    //printf("\n1. Add\n2. Subtract\n3. Multiply\n4. Divide\n5. Factorial\n6. Armstrong\n7. Primeno check\n8. Square Root");
+    //printf("\n1. Add\n2. Subtract\n3. Multiply\n4. Divide\n5. Factorial\n6. Armstrong\n7. Prime no check\n8. Square Root");
     //printf("\n17. Exit");
     switch(choice)
     {
@@ -137,7 +137,7 @@ void calculator_menu(void)
             
         case 3:
         {
-            printf("\n1. Armstrong\n2. Primeno check\n3. exit");
+            printf("\n1. Armstrong\n2. Prime no check\n3. exit");
             printf("\n\tEnter your choice\t");
    
      
